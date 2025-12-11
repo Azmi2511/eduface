84e4cbfdd6b1a49d282e460c03c4c33d320bb55d
Azmi2511 <zulazmy07@gmail.com>
2025-12-11 13:28:20 +0700

    Penyesuaian logo dan title pada sidebar agar dinamis menyesuaikan nama sekolah

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,16 +1,21 @@
 @php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
     $active = $active_menu ?? '';
     $master_data_pages = ['users','students','teachers','parents','classes'];
     $is_master_open = in_array($active, $master_data_pages);
+    $school_name = DB::table('system_settings')->value('school_name');
 @endphp
 <aside class="w-64 bg-[#3B82F6] text-white flex-shrink-0 hidden md:flex flex-col transition-all duration-300">
     <div class="h-20 flex items-center px-6 py-4 border-b border-blue-400/30">
-        <div class="w-10 h-10 bg-white rounded-lg flex items-center justify-center shadow-sm mr-3">
+        <div class="w-8 h-8 bg-white rounded-lg flex items-center justify-center shadow-sm mr-3">
             <img src="{{ asset('assets/logo.png') }}" alt="logo" class="w-9 h-9 object-contain">
         </div>
         <div>
-            <h1 class="text-md font-bold leading-tight">EduFace</h1>
-            <p class="text-xs text-blue-100">Sistem Absensi</p>
+            <h1 class="text-md font-bold leading-tight">EduFace: Just Face It</h1>
+            <p class="text-xs text-blue-100">{{ $school_name }}</p>
         </div>
     </div>
 
