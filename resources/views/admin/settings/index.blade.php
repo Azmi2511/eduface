152328aa5a19f1e4b5d0a69e05a611fb72ddae47
Azmi2511 <zulazmy07@gmail.com>
2025-12-17 09:58:41 +0700

    Update Tampilan

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -11,22 +11,6 @@ $active_menu = 'settings';
 <div class="flex-1 flex flex-col overflow-hidden bg-[#F3F6FD]">
 
     <main class="flex-1 overflow-y-auto p-8">
-        
-        {{-- Flash Message --}}
-        @if(session('success'))
-            <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative mb-4" role="alert">
-                <span class="block sm:inline">{{ session('success') }}</span>
-            </div>
-        @endif
-        @if($errors->any())
-            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
 
         <div class="flex flex-col lg:flex-row gap-6">
 
